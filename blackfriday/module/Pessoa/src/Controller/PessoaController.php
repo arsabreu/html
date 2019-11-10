f0a0c755c9feb4cbc770f66f3e7eb54af7db17d9
arsabreu <arsabreu@gmail.com>
2019-11-10 20:11:14 -0300

    crud em codeIgniter

---
+++ blackfriday/module/Pessoa/src/Controller/PessoaController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Pessoa\Controller;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+
+class PessoaController extends AbstractActionController {
+
+    private $table;
+
+    public function __construct($table){
+        $this->table = $table;
+    }
+
+    public function indexAction() {
+        return new ViewModel(['pessoas' => $this->table->getAll()]);
+    }
+
+    public function adicionarAction() {
+        return new ViewModel();
+    }
+
+    public function salvarAction() {
+        return new ViewModel();
+    }
+
+    public function editarAction() {
+        return new ViewModel();
+    }
+
+    public function removerAction() {
+        return new ViewModel();
+    }
+    
+    public function confirmacaoAction() {
+        //return new ViewModel();
+    }
+}
